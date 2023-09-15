7c6d5b5f131fbff12407a77cc23219723ce8b8dc
R3nnyWeb <nikitavaskulatov@gmail.com>
2023-09-15 18:00:59 +0300

    Tree struck

---
+++ Algs/labTree/Tree.h
@@ -0,0 +1,13 @@
+//
+// Created by R3nny on 15.09.2023.
+//
+#pragma once
+namespace tree {
+    typedef int data_type;
+    struct Node{
+        data_type data;
+        Node* left;
+        Node* right;
+    };
+
+}
