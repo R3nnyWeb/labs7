63403e9ac29cda5a0ec029ae3dde295e4da5816a
Slava <Anohin.VA@yandex.ru>
2023-09-20 11:45:15 +0300

    metods

---
+++ CortexM3C/Текст_программ/ПР4LCD/1986ВЕ92QI.c
@@ -0,0 +1,104 @@
+#include "MDR32F9Qx_port.h"			//библиотека работы с портами
+#include "MDR32F9Qx_rst_clk.h"	//библиотека работы с модулем тактирования
+#include "mlt_lcd.h"						//библиотека работы с дисплеем
+
+//структура инициализации порта
+PORT_InitTypeDef PortStruct;
+
+void LCDPins (void) //процедура инициализации выводов МК для работы с LCD
+{ //все линии цифровые в режиме PORT с медленным фронтом
+	//линии DB5:DB0 - PA5:PA0 по умолчанию ввод без подтяжек и фильтров
+	PortStruct.PORT_Pin = PORT_Pin_0|PORT_Pin_1|PORT_Pin_2|PORT_Pin_3|PORT_Pin_4|PORT_Pin_5;
+	PortStruct.PORT_OE = PORT_OE_IN;
+	PortStruct.PORT_FUNC = PORT_FUNC_PORT;
+	PortStruct.PORT_SPEED = PORT_SPEED_SLOW;
+	PortStruct.PORT_MODE = PORT_MODE_DIGITAL;
+	PORT_Init(MDR_PORTA,&PortStruct);
+	//линии DB7:DB6 - PF3:PF2 по умолчанию ввод без подтяжек и фильтров
+	PortStruct.PORT_Pin = PORT_Pin_2|PORT_Pin_3;
+	PORT_Init(MDR_PORTF,&PortStruct);
+	//линии E1 - PB7, E2 - PB8,RES - PB9, R/~W - PB10 вывод
+	PortStruct.PORT_OE = PORT_OE_OUT;
+	PortStruct.PORT_Pin = PORT_Pin_7|PORT_Pin_8|PORT_Pin_9|PORT_Pin_10;
+	PORT_Init(MDR_PORTB,&PortStruct);
+	//Линии A0 - PC0, E - PC1 вывод
+	PortStruct.PORT_Pin = PORT_Pin_0|PORT_Pin_1;
+	PORT_Init(MDR_PORTC,&PortStruct);
+}
+
+void CPUinit (void)				//процедура инициализации внешнего тактового генератора для процессора
+{
+	//включение внешнего высокочастотного генератора
+	RST_CLK_HSEconfig(RST_CLK_HSE_ON);
+	//запрос состояния, только если готов, то переход к следующим действиям
+	while (RST_CLK_HSEstatus()!=SUCCESS);
+	//включение модуля PLL
+	RST_CLK_CPU_PLLcmd(ENABLE);
+	// умножение частоты кварцевого генератора в 10 раз
+	RST_CLK_CPU_PLLconfig(RST_CLK_CPU_PLLsrcHSEdiv1, RST_CLK_CPU_PLLmul6);
+	//запрос состояния PLL, если готов, то переход к следующим действиям 
+	while (RST_CLK_CPU_PLLstatus()!=SUCCESS);	
+	//переключение на частоту сформированную PLL
+	RST_CLK_CPU_PLLuse(ENABLE);
+	RST_CLK_CPUclkPrescaler(RST_CLK_CPUclkDIV1);
+	//переключение на тактовый сигнал CPU_C3
+	RST_CLK_CPUclkSelection(RST_CLK_CPUclkCPU_C3);
+}
+
+void PCLKinit(void)														//процедура тактирования портов
+{
+	RST_CLK_PCLKcmd(RST_CLK_PCLK_PORTA,ENABLE);	//включение тактирования PORTA
+	RST_CLK_PCLKcmd(RST_CLK_PCLK_PORTB,ENABLE);	//включение тактирования PORTB
+	RST_CLK_PCLKcmd(RST_CLK_PCLK_PORTC,ENABLE);	//включение тактирования PORTC
+	RST_CLK_PCLKcmd(RST_CLK_PCLK_PORTF,ENABLE);	//включение тактирования PORTF
+}
+
+void LCDStart(void)							//процедура запуска дисплея
+{
+	uint8_t status;								//переменная для анализа состояния чипа
+	LcdInit(); //установка сигнала перезагрузки, ожидание завершения операции
+	do 
+	{
+		status = ReadStatus(1);//чтение состояния первого чипа
+	}while ((status&0x80)!=0x00); //пока не готов снова прочитать состояние
+	do
+	{
+				status = ReadStatus(2);//чтение состояния второго чипа
+		}while((status&0x80)!=0x00); //пока не готов снова прочитать состояние
+	DispOn (1);										//включение чипа 1
+	DispOn (2);										//включение чипа 2	
+	LcdClearChip(1);							//очистка чипа 1
+	LcdClearChip(2);							//очистка чипа 2
+}
+
+void LCDTestPrint(void)
+{
+	SetPage(1,2);					//установить 2 страницу 1-ого чипа
+	SetAdress(1,5);				//установить адрес - 5, для 1-ого чипа, (2 страницы)
+	WriteData (1,0x20);		//вывод на экран точки с положением 0x20 (по адресу 5, 2 страница, чип 1)
+	SetPage(2,3);					//установить 3 страницу 2-ого чипа
+	SetAdress(2,0);				//установить адрес - 0, для 2-ого чипа, (3 страницы)
+	for (int i=0;i<7;i++)	//перебор комбинаций от 0 до 7
+		{
+			WriteData(2,i);		//вывод на экран точки с положением i, адрес увеличивается на 1 после выполнения каждой команды
+		}
+	SetPage(2,4);					//установить 4 страницу 2-ого чипа
+	SetAdress(2,0);				//установить адрес - 0, для 2-ого чипа, (4 страницы)
+	for (int i=0;i<63;i++)	//перебор комбинаций от 0 до 63
+		{
+			WriteData(2,i);		//вывод на экран точки с положением i, адрес увеличивается на 1 после выполнения каждой команды
+		}
+}
+
+int main (void)		//точка входа в программу	
+{
+	CPUinit();			//настройка тактирования процессора
+	PCLKinit();			//включние тактовых сигналов
+	LCDPins();			//инициализация выводов
+	LCDStart();			//запуск LCD
+	LCDTestPrint();	//вывод тестовых значений на экран
+	while(1)				//бесконечный цикл
+		{
+
+		}
+}
---
+++ OpenCL/lab1/lab1/lab1.cpp
@@ -0,0 +1,50 @@
+﻿// lab1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+#include <CL/cl.h>
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+void Init() {
+    cl_uint numPlatforms;
+    cl_platform_id platform = NULL;
+    cl_int status = clGetPlatformIDs(0, NULL, &numPlatforms);
+    #define CL_SUCCESS 0
+    if (status != CL_SUCCESS)
+    {
+        //Обрабатываем ошибку
+        const char* output = "Ошибка";
+        cout << output << endl;
+    }
+    if (numPlatforms > 0)
+    {
+        cl_platform_id* platforms = (cl_platform_id*)malloc(numPlatforms * sizeof(cl_platform_id));
+        status = clGetPlatformIDs(numPlatforms, platforms, NULL);
+        platform = platforms[0];
+        free(platforms);
+    }
+
+    
+}
+
+int main()
+{
+    Init;
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
