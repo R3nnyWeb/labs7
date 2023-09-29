1bfee71de3b453b4fa6e0ff3215e27bee29dc7e6
R3nnyWeb <nikitavaskulatov@gmail.com>
2023-09-29 17:31:40 +0300

    Add insert

--- Algs/labTree/Node.cpp
+++ Algs/labTree/Node.cpp
@@ -4,20 +4,26 @@
 #include "Node.h"
 
 namespace tree {
+    void INSERT(const data_type *data, Node &root, int i);
+
     Node *MAKE_TREE() {
-        data_type data[] = {2, 5, 6, 1, 6, 7, 2};
+        data_type data[] = {2, 5, 6, 1, -1, 7, 2};
         int n = 7;
         Node root = Node(data[0]);
 
         for (int i = 1; i < n; i++) {
-            Node *current = &root;
-            data_type currentData = data[i];
-            while (current != nullptr) {
-                if (currentData < root.data)
-                    current = LEFT(current);
-                else current = RIGHT(current);
-            }
-
+            INSERT(&root, data[i]);
         }
+    }
+
+    Node* INSERT(Node *tree, data_type data) {
+        if (tree == nullptr) {
+            return new Node(data);
+        } else if (data < tree->data) {
+            tree->left = INSERT(tree->left, data);
+        } else if (data > tree->data) {
+            tree->right = INSERT(tree->right, data);
+        } else throw "Repeat keys";
+        return tree;
     }
 }
