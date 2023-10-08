b41bfcf66a848a1f5480ed80300271255fd4d3c8
R3nnyWeb <nikitavaskulatov@gmail.com>
2023-10-08 18:20:27 +0300

    PIS 8, сруктуры немного

--- Algs/labTree/Node.cpp
+++ Algs/labTree/Node.cpp
@@ -8,10 +8,7 @@ using namespace std;
 namespace tree {
 
 
-    Node *MAKE_TREE() {
-        data_type data[] = {2, 5, 6, 1, -1, 7, 9};
-
-        int n = 7;
+    Node *MAKE_TREE(data_type data[], int n) {
         Node *root = new Node(data[0], 0);
 
         for (int i = 1; i < n; i++) {
@@ -43,14 +40,17 @@ namespace tree {
     data_type INFO(Node *node) {
         return node->data;
     }
-
-    data_type FIND(data_type key, Node *tree) {
+    int FIND_IN_TREE(data_type key, Node *tree){
         if (tree == nullptr) return -1;
         else if (INFO(tree) == key)
             return tree->index;
         else if (key < INFO(tree))
-            FIND(key, LEFT(tree));
-        else FIND(key, RIGHT(tree));
+            FIND_IN_TREE(key, LEFT(tree));
+        else FIND_IN_TREE(key, RIGHT(tree));
+    }
+    int FIND(data_type key, data_type data[], int n) {
+        Node* tree = MAKE_TREE(data, n);
+        return FIND_IN_TREE(key, tree);
     }
 
     void TLR(Node *tree, int level) {
@@ -60,14 +60,16 @@ namespace tree {
         TLR(tree->left, level + 1);
         TLR(tree->right, level + 1);
     }
-    void LTR(Node* tree, int level) {
+
+    void LTR(Node *tree, int level) {
         if (tree == nullptr) return;
         LTR(tree->left, level + 1);
         for (int i = 0; i < level; i++) cout << "=";
         cout << "> " << tree->data << endl;
         LTR(tree->right, level + 1);
     }
-    void LRT(Node* tree, int level) {
+
+    void LRT(Node *tree, int level) {
         if (tree == nullptr) return;
         LRT(tree->left, level + 1);
         LRT(tree->right, level + 1);
