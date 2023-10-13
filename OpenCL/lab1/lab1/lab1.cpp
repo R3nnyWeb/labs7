c697e4066892339407f06ef48d70b842200fb49b
Slava <Anohin.VA@yandex.ru>
2023-10-13 17:03:14 +0300

    openCL

--- OpenCL/lab1/lab1/lab1.cpp
+++ OpenCL/lab1/lab1/lab1.cpp
@@ -42,42 +42,74 @@ int convertToString(const char* filename, std::string& s)
 	return -1;
 }
 
+void isSuccess(cl_int status) {
+	#define CL_SUCCESS 0
+	if (status != CL_SUCCESS)
+	{
+		cout << "Ошибка: " << status << endl;
+	}
+}
+
 void Init() {
     cl_uint numPlatforms;
     cl_platform_id platform = NULL;
     cl_int status = clGetPlatformIDs(0, NULL, &numPlatforms);
-    #define CL_SUCCESS 0
-    if (status != CL_SUCCESS)
-    {
-        //Обрабатываем ошибку
-        const char* output = "Ошибка";
-        cout << output << endl;
-    }
+	isSuccess(status);
 	cout << "NumPlatforms: " << numPlatforms << endl;
 
     if (numPlatforms > 0)
     {
         cl_platform_id* platforms = (cl_platform_id*)malloc(numPlatforms * sizeof(cl_platform_id));
         status = clGetPlatformIDs(numPlatforms, platforms, NULL);
+		isSuccess(status);
         platform = platforms[0];
         free(platforms);
     }
     cl_uint numDevices = 0;
     cl_device_id* devices;
     status = clGetDeviceIDs(platform, CL_DEVICE_TYPE_GPU, 0, NULL, &numDevices);
+	isSuccess(status);
 	cout << "NumDevices: " << numDevices << endl;
 
-    devices = (cl_device_id*)malloc(numDevices * sizeof(cl_device_id));
-   status = clGetDeviceIDs(platform, CL_DEVICE_TYPE_GPU, numDevices, devices, NULL);
+	devices = (cl_device_id*)malloc(numDevices * sizeof(cl_device_id));
+	status = clGetDeviceIDs(platform, CL_DEVICE_TYPE_GPU, numDevices, devices, NULL);
+	isSuccess(status);
+	for (int i = 0; i < numDevices; i++) {
+		printf("  Device number: %d\n", i);
+		char buffer[110];
+		status = clGetDeviceInfo(devices[i], CL_DEVICE_NAME, 100, buffer, NULL);
+		printf("Device name: %s\n", buffer);
+		// Запрос максимального количества вычислительных единиц устройства
+		cl_uint UnitNum;
+		status = clGetDeviceInfo(devices[i], CL_DEVICE_MAX_COMPUTE_UNITS, sizeof(cl_uint), &UnitNum, NULL);
+		printf("Compute Units Number: %d\n", UnitNum);
+		// Запрос частоты ядра устройства
+		cl_uint frequency;
+		status = clGetDeviceInfo(devices[i], CL_DEVICE_MAX_CLOCK_FREQUENCY, sizeof(cl_uint), &frequency, NULL);
+		printf("Device Frequency: %d(MHz)\n", frequency);
+		// Запрос общего объема памяти устройства
+		cl_ulong GlobalSize;
+		status = clGetDeviceInfo(devices[i], CL_DEVICE_GLOBAL_MEM_SIZE, sizeof(cl_ulong), &GlobalSize, NULL);
+		printf("Device Global Size: %0.0f(MB)\n", (float)GlobalSize / 1024 / 1024);
+		// Запрос строки глобального кэша памяти устройства
+		cl_uint GlobalCacheLine;
+		status = clGetDeviceInfo(devices[i], CL_DEVICE_GLOBAL_MEM_CACHELINE_SIZE, sizeof(cl_uint), &GlobalCacheLine, NULL);
+		printf("Device Global CacheLine: %d(Byte)\n", GlobalCacheLine);
+		// Запрос версии OpenCL, поддерживаемой устройством
+		char DeviceVersion[110];
+		status = clGetDeviceInfo(devices[i], CL_DEVICE_VERSION, 100, DeviceVersion, NULL);
+		printf("Device Version:%s\n", DeviceVersion);
+	}
+
+    
 	/*char openclVersion[1024];
 	status = clGetDeviceInfo(devices[0], CL_DEVICE_OPENCL_C_VERSION,
 		sizeof(openclVersion), openclVersion, 0);
 	cout << openclVersion << endl;*/
 
-	char deviceName[1024];
-	status = clGetDeviceInfo(devices[0], CL_DEVICE_TYPE,
-		sizeof(deviceName), deviceName, 0);
-	cout << deviceName << endl;
+	/*char deviceName[1024];
+	status = clGetDeviceInfo(devices[0], CL_DEVICE_TYPE, sizeof(deviceName), deviceName, 0);
+	cout << deviceName << endl;*/
 
 	cl_context context = clCreateContext(NULL, 1, devices, NULL, NULL, NULL);
 	cl_command_queue commandQueue = clCreateCommandQueue(context, devices[0], 0, NULL);
@@ -85,7 +117,7 @@ void Init() {
 
 
 
-
+	cout << endl << "Run programs" << endl << endl;
 	const char* filename = "HelloWorld_Kernel.cl";
 	HelloWOrld(filename, context, devices, commandQueue);
 	const char* filename1 = "Mul.cl";
@@ -160,10 +192,15 @@ void Mul(const char* filename, const cl_context& context, cl_device_id* devices,
 	cl_program program = clCreateProgramWithSource(context, 1, &source, sourceSize, NULL);
 	status = clBuildProgram(program, 1, devices, NULL, NULL, NULL);
 
-	const size_t size = 10;
-
-	const float input1[size] = {2.0f,2.0f,3.0f,4.0f,5.0f,6.0f,7.0f,8.0f,9.0f,10.0f};
-	const float input2[size] = {41.0f,2.0f,3.0f,4.0f,5.0f,6.0f,7.0f,8.0f,9.0f,10.0f };
+	const size_t size = 100;
+	
+	//{1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f, 9.0f, 10.0f }
+	float input1[size];
+	float input2[size];
+	for (size_t i = 1; i <= size; i++) {
+		input1[i-1] = (float)i;
+		input2[i-1] = (float)(i+1);
+	}
 
 
 
@@ -191,10 +228,10 @@ void Mul(const char* filename, const cl_context& context, cl_device_id* devices,
 
 	for (size_t i = 0; i < size; i++)
 	{
-		cout << output[i] << endl;
+		cout << output[i] << "  ";
 	}
+	cout << endl;
 	
-
 
 	status = clReleaseKernel(kernel);
 	status = clReleaseProgram(program);
@@ -209,14 +246,3 @@ int main()
     Init();
 	return 0;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
